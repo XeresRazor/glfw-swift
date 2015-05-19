f2f254099390241df8a602cd60622de4d03932d5
David Green <shinohara@gmail.com>
2015-05-19 12:58:44 -0400

    Initial Commit

---
+++ glfw-swift/glfw-swift.h
@@ -0,0 +1,19 @@
+//
+//  glfw-swift.h
+//  glfw-swift
+//
+//  Created by Articulate on 5/19/15.
+//  Copyright (c) 2015 DigitalWorlds. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for glfw-swift.
+FOUNDATION_EXPORT double glfw_swiftVersionNumber;
+
+//! Project version string for glfw-swift.
+FOUNDATION_EXPORT const unsigned char glfw_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <glfw_swift/PublicHeader.h>
+
+
